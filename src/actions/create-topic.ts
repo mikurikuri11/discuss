22c8b29fda2674980d4a2fd585f5328101d7817d
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-09-30 22:32:03 +0900

    ✨ フォーム作成完了’

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,40 @@
 "use server";
 
-export async function createTopic() {
-  // TODO: revalidate the homepage after creating a topic
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error?.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
 }
